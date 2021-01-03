2b2684238caa346ee442e014b5b827556b3e4ab4
etozhenerk <etozhenerk@yandex.ru>
2021-01-03 16:50:05 +0300

    Набросок верстки, первоначальный функционал

--- script.js
+++ script.js
@@ -24,12 +24,13 @@ const proection = (fields, obj) =>
     }, {});
 
 const renderMovies = ([...arr]) => {
-  const moviesList = document.querySelector(".movies-list");
+  const moviesList = document.querySelector(".options-container");
   arr.sort().forEach((movie) => {
-    const li = document.createElement("li");
-    li.classList.add("movies-list__item");
-    li.innerHTML = `<span class="movies-list__item-text">${movie}</span>`;
-    moviesList.append(li);
+    const option = document.createElement("div");
+    option.classList.add("option");
+    option.innerHTML = `<input type="radio" class="radio" id="${movie}" name="category">
+                          <label for="${movie}">${movie}</label>`;
+    moviesList.append(option);
   });
 };
 const renderCards = (arr) => {
@@ -40,59 +41,135 @@ const renderCards = (arr) => {
     card.classList.add("col-7");
     card.classList.add("col-sm-6");
     card.classList.add("col-md-4");
-    card.classList.add("col-lg-3");
+    card.classList.add("col-lg-4");
     card.innerHTML = `
-              <div class="heroes-item">
+              <div class="heroes-item" data-name="${item.name}">
                 <div class="heroes-item__img">
                   <img src="./${item.photo}" alt="">
                 </div>
                 <div class="heroes-item__descr">
-                  <div class="heroes-item__hero-name"><span id="name">${item.name}</span>
+                  <div class="heroes-item__hero-name"><span id="name">${
+                    item.name
+                  }</span>
                   </div>
                   <div class="heroes-item__text">
-                    <div class="heroes-item__real-name"><u><b>Real name:</b></u> <span id="realName">${item.realName}</span></div>
-                    <div class="heroes-item__movies"><u><b>Movies:</b></u><br></div>
-                    <div class="heroes-item__status"><u><b>Hero Status:</b></u> <span id="status">${item.status}</span>
+                    <div class="heroes-item__real-name"><u><b>Real name:</b></u> <span>${
+                      item.realName || "Реальное имя не известно"
+                    }</span></div>
+                    <div class="heroes-item__status"><u><b>Hero Status:</b></u> <span>${
+                      item.status
+                    }</span>
                   </div>
                   </div>
                 </div>
               </div>
     `;
     heroesList.append(card);
-
-    if(item.movies){
-      item.movies.forEach(movie => {
-        const span = document.createElement('span');
-        span.innerHTML = `${movie} <br>`;
-        card.querySelector('.heroes-item__movies').append(span);
-      });
-    } else {
-      const span = document.createElement('span');
-      span.innerHTML = `В фильмах не появлялся`;
-        card.querySelector('.heroes-item__movies').append(span);
-    }
-
   });
 };
 
+const changeOption = () => {
+  const selected = document.querySelector(".selected"),
+    optionsContainer = document.querySelector(".options-container");
+
+  selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+  });
+
+  optionsContainer.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.matches(".option")) {
+      selected.innerHTML = target.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+    }
+  });
+};
+
+changeOption();
+
+const renderPopup = (name) => {
+  const heroName = document.querySelector(".hero-name"),
+    realName = document.querySelector(".real-name"),
+    actorName = document.querySelector(".actor-name"),
+    status = document.querySelector(".status"),
+    race = document.querySelector(".race"),
+    citizenship = document.querySelector(".citizenship"),
+    movies = document.querySelector(".movies"),
+    heroImg = document.querySelector(".hero-img");
+  getData((data) => {
+    const heroesCard = data.map((item) =>
+      proection(
+        [
+          "name",
+          "realName",
+          "actors",
+          "citizenship",
+          "species",
+          "photo",
+          "movies",
+          "status",
+        ],
+        item
+      )
+    );
+    heroesCard.forEach((hero) => {
+      if (hero.name === name) {
+        heroName.textContent = hero.name;
+        realName.textContent = hero.realName || "Name unknown";
+        actorName.textContent = hero.actors;
+        status.textContent = hero.status;
+        race.textContent = hero.species;
+        citizenship.textContent = hero.citizenship || "Nationality unknown";
+        movies.textContent = hero.movies;
+        heroImg.src = `./${hero.photo}`;
+      }
+    });
+  });
+};
+
+const togglePopup = () => {
+  const heroesList = document.querySelector(".heroes-list"),
+    popup = document.querySelector(".overlay"),
+    body = document.querySelector("body");
+
+  heroesList.addEventListener("click", (event) => {
+    const target = event.target.closest(".heroes-item");
+    if (target) {
+      renderPopup(target.dataset.name);
+      setTimeout(() => {
+        popup.classList.add("overlay-active");
+        body.style.overflow = "hidden";
+      }, 100);
+    }
+  });
+
+  popup.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target.matches(".overlay") || target.matches(".popup-close")) {
+      popup.classList.remove("overlay-active");
+      body.style.overflow = "auto";
+    }
+  });
+};
+
+togglePopup();
+
 const getMovies = (data) => {
   const movies = new Set();
-  const newHeroes = data.map((item) => proection(["movies"], item));
-  newHeroes.forEach((elem) => {
+  const movie = data.map((item) => proection(["movies"], item));
+  movie.forEach((elem) => {
     if (elem.movies) {
       elem.movies.forEach((movie) => {
         movies.add(movie);
       });
     }
   });
-  renderMovies(movies);
-};
-const getHeroes = (data) => {
-  const newHeroes = data.map((item) =>
+  const heroesCard = data.map((item) =>
     proection(["name", "realName", "photo", "movies", "status"], item)
   );
-  renderCards(newHeroes);
+  renderCards(heroesCard);
+  renderMovies(movies);
 };
 
 getData(getMovies);
-getData(getHeroes);
